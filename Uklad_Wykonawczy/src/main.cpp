440e6a525471d51fadb3626934da7b62ac1f3a33
JSzyba <148805995+JSzyba@users.noreply.github.com>
2024-11-26 14:48:13 +0100

    Executive revision 2

--- Uklad_Wykonawczy/src/main.cpp
+++ Uklad_Wykonawczy/src/main.cpp
@@ -5,6 +5,9 @@ const int LED = 3;
 const int BASE_SERVO = 5;
 const int UPPER_SERVO = 6;
 
+unsigned long previousMillis = 0;
+const unsigned long interval = 500;
+
 Servo base_servo;
 Servo upper_servo;
 
@@ -14,51 +17,53 @@ void setup() {
   upper_servo.attach(UPPER_SERVO);
 
   pinMode(LED, OUTPUT);
-  base_servo.write(90); // Ustawienie domyślnej pozycji
-  upper_servo.write(90); // Ustawienie domyślnej pozycji
+  base_servo.write(90); // Setting default position
+  upper_servo.write(90);
 
-  // delay(50000);
+  // delay(50000); //For assembling the device for the first time with resetting servo position
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    char input = Serial.read(); // Odczytaj pierwszy znak
-    int value = Serial.parseInt(); // Odczytaj liczbę całkowitą
+    char input = Serial.read(); // Read first char
+    int value = Serial.parseInt(); // Read integer
 
-    Serial.print("Command: ");
-    Serial.println(input);
-    Serial.print("Value: ");
-    Serial.println(value);
+    // Serial.print("Command: ");
+    // Serial.println(input);     //For debug purposes
+    // Serial.print("Value: ");
+    // Serial.println(value);
 
     switch (input) {
-      case 'b': // Ustawienie pozycji base_servo
+      case 'b': // Base servo
         if (value >= 0 && value <= 180) {
           base_servo.write(value);
-        } else {
-          Serial.println("Base servo value out of range (0-180).");
         }
         break;
 
-      case 'u': // Ustawienie pozycji upper_servo
+      case 'u': //upper_servo
         if (value >= 20 && value <= 160) {
           upper_servo.write(value);
-        } else {
-          Serial.println("Upper servo value out of range (20-160).");
         }
         break;
 
-      case 'l': // Ustawienie jasności LED
+      case 'l': // LED brightness
         if (value >= 0 && value <= 255) {
           analogWrite(LED, value);
-        } else {
-          Serial.println("LED brightness value out of range (0-255).");
         }
         break;
 
       default:
-        Serial.println("Invalid command. Use 'b', 'u', or 'l'.");
+        Serial.println("Incorrect Info");
         break;
     }
   }
-  delay(5000);
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    Serial.println("Arduino Alive"); //Heartbeat of the arduino to signalise the central_unit that the executive_components are working
+  }
+
+  delay(200);
 }
